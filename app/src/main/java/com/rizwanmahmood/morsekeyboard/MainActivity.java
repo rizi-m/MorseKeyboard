4222767179f03a610a289f624ef04f4883972c69
Rizwan Mahmood <rizwanmahmood255@gmail.com>
2017-05-29 20:07:13 +0100

    added help page to aid users in enabling the keyboard and a button for a learninig functionality that will be added soon

--- app/src/main/java/com/rizwanmahmood/morsekeyboard/MainActivity.java
+++ app/src/main/java/com/rizwanmahmood/morsekeyboard/MainActivity.java
@@ -1,13 +1,38 @@
 package com.rizwanmahmood.morsekeyboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.rizwanmahmood.morsekeyboard.activities.HelpActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialise();
+        setup();
+    }
+
+    //setup everything needed... event etc.
+    private void setup() {
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(btnHelp.getContext(), HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //initialise all the items
+    private void initialise() {
+        btnHelp = (Button) findViewById(R.id.main_btn_help);
     }
 }
